fcf173a384d65a53daae9d76b204dbc407520f29
Ulrich-Bertrand <ulrichbertrandmoffo@gmail.com>
2022-03-05 13:12:01 +0100

    relationship: one to many

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,4 +10,13 @@ class Post extends Model
     use HasFactory;
 
     protected $fillable = ['title', 'content'];
+
+    /**
+     * Get the comments for the blog post.
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
 }
